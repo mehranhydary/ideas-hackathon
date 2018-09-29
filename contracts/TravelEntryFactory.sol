2c1da8a17473cbf7eb4dad79c1a508cabd651bac
Ashwin <askala@deloitte.ca>
2018-09-29 19:06:19 -0400

    mergemaster

--- contracts/TravelEntryFactory.sol
+++ contracts/TravelEntryFactory.sol
@@ -4,8 +4,8 @@ import  "./PassportFactory.sol";
 contract TravelEntryFactory is PassportFactory {
     struct TravelHistory{
         bytes32 location;
-        bytes32 entryDate;
-        bytes32 exitDate;
+        uint256 entryDate;
+        uint256 exitDate;
         address issuingAuth;
     }
     
@@ -13,15 +13,15 @@ contract TravelEntryFactory is PassportFactory {
 
     function createTravelHistory(
         bytes32 _location,
-        bytes32 _entryDate,
+        uint256 _entryDate,
         address _traveller) public {
         TravelHistory memory travelHistory = TravelHistory(_location, _entryDate, 0,msg.sender);
         travels[passports[_traveller].passportNumber].push(travelHistory);
     }
 
     function updateTravelHistory(
-        bytes32 _exitDate,
+        uint256 _exitDate,
         address _traveller) public {
-        travels[passports[_traveller].passportNumber][travels[passports[_traveller].passportNumber].length-1].exitDate = _exitDate;
+        travels[passports[_traveller].passportNumber][travels[passports[_traveller].passportNumber].length - 1].exitDate = _exitDate;
     }
 }
